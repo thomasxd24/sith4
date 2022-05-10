45abe36d20e9157d9ce46a5784e88702d40ebfee
Julien Constant <julienconstant190@gmail.com>
2022-05-10 23:17:58 +0200

    moved i18n into /plugins & fix locales(key)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import i18n from "./i18n";
+import i18n from "./plugins/i18n";
 
 createApp(App).use(i18n).use(router).mount("#app");
